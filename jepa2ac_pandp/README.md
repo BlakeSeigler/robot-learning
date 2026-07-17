8fddccd4763489fc941c5f665b365b60d8da5a05
BlakeSeigler <seiglerblake@gmail.com>
2026-07-16 21:52:50 -0400

    stuff.

---
+++ jepa2ac_pandp/README.md
@@ -0,0 +1,3 @@
+command to get weights
+
+wget https://dl.fbaipublicfiles.com/vjepa2/vjepa2-ac-vitg.pt -P ./checkpoints/ 
---
+++ jepa2ac_pandp/download_model.py
@@ -0,0 +1,4 @@
+import torch
+
+vjepa2_encoder, vjepa2_ac_predictor = torch.hub.load('facebookresearch/vjepa2', 'vjepa2_ac_vit_giant')
+
